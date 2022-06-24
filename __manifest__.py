ff53f57d53f707079d4129f3dc281a7c0c8a5f95
David Moreno <david.moreno@alliantum.com>
2022-06-24 11:11:02 +0200

    [IMP] make image sequence to be visible and editable from the form view too

--- __manifest__.py
+++ __manifest__.py
@@ -10,7 +10,7 @@
     'website': "https://www.alliantum.com/",
     'category': 'Inventory',
     'license': 'AGPL-3',
-    'version': '14.0.1.1.1',
+    'version': '14.0.1.1.2',
     'depends': ['product', 'stock', 'mrp'],
     'data': [
         'security/ir.model.access.csv',
